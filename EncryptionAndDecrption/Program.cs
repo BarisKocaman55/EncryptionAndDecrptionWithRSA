862a9d26d0eee11352b1890f8ce0b2bab0ff85eb
baris <baris@DESKTOP-08HI88N>
2023-02-07 20:55:51 +0300

    Add project files.

---
+++ EncryptionAndDecrption/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EncryptionAndDecrption
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
